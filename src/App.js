c4fb18922980ae6598b44ea67f27bf5f9ae71e29
subir <subir.ksoftware@gmail.com>
2022-04-26 23:48:32 +0530

    search function added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { Table, Container, Row, Col,Form,Button} from "react-bootstrap";
+const App = () => {
+  const [data, setData] = useState([]);
+  const [value,setValue]= useState('');
+  useEffect(() => {
+    loadData();
+  }, []);
 
-function App() {
+  const loadData = async () => {
+    return await axios
+      .get("http://localhost:5000/member")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  };
+ 
+  const loadSearch = async(e)=>{
+    e.preventDefault()
+    return await axios
+      .get(`http://localhost:5000/member?q=${value}`)
+      .then((res) => 
+      setData(res.data),
+      setValue('')
+      )
+      .catch((err) => console.log(err));
+  }
+  const loadReset = ()=>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container style={{ marginTop: "100px" }}>
+        <Row>
+          <Col lg={4} md={12} sm={12}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="text" placeholder="Search By Name" value={value} onChange={(e)=>setValue(e.target.value)} />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={loadSearch}>
+                Search
+              </Button>
+              <Button variant="danger" onClick={loadReset}>
+                Reset
+              </Button>
+            </Form>
+          </Col>
+          <Col lg={8} md={12} sm={12}>
+            <h3 className="text-center">Search,Sort,Pagination Example</h3>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Class</th>
+                  <th>Roll</th>
+                  <th>Game</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              {data.length === 0?<h2>No Data Available</h2>:
+              data?.map((i,index)=>(
+              <tbody key={index}>
+                <tr>
+                  <td>{i.id}</td>
+                  <td>{i.name}</td>
+                  <td>{i.class}</td>
+                  <td>{i.roll}</td>
+                  <td>{i.game}</td>
+                  <td>{i.status}</td>
+                </tr>
+              </tbody>
+              ))}
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
